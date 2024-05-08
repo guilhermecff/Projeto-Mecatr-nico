86fe060e1048c21b39dbd565b8f1906bbccdcc43
Guilherme Chicharo Froes da Fonseca <guiga.froes3@gmail.com>
2024-05-08 13:43:58 +0000

    Jog todos eixos

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "mbed.h"
 
+
 Ticker toggle;
 // PARA X
 DigitalOut StepDriverXY(D11);
@@ -13,7 +14,13 @@ InterruptIn fdcY1(D5); // ymax
 // PARA Z
 DigitalOut direcaoZ(D14);
 DigitalOut enableZ(D15);
-InterruptIn fdcZ1(D4); // zmax*/
+InterruptIn fdcZ1(D4); // zmax
+
+DigitalIn BotaoZcima(PC_11);
+DigitalIn BotaoZbaixo(PC_10);
+
+
+
 AnalogIn EixoXJoyStick(A0);
 AnalogIn EixoYJoyStick(A1);
 
@@ -74,21 +81,22 @@ void refEixoY(){
     }
 
 }
+
 void refEixoZ(){
     
     enableZ=1;
     direcaoZ=0;
     while(!referenciadoZ){
-        if(fdcX1==0){
+        if(fdcZ1==0){
             referenciadoZ=true;
             break;
         }
         enableZ=0;
     }
-    if (referenciadoX){
+    if (referenciadoZ){
         direcaoZ=!direcaoZ;
         wait(0.2);
-        enableX=1;
+        enableZ=1;
     }
 
 }
@@ -98,7 +106,8 @@ void jog(){
     joyX = EixoXJoyStick.read() * 1000;
     joyY = EixoYJoyStick.read() * 1000;
 
-    if (joyX<450){
+    // PARA X
+    if (joyX<430){
         direcaoX=1;
         enableX=0;
         if (StepDriverXY==1){
@@ -115,11 +124,13 @@ void jog(){
         }
             
     }
-    if(joyX<550 && joyX>450){
+    if(joyX<550 && joyX>430){
         enableX=1;
     }
 
-    if (joyY<450){
+    //PARA Y
+
+    if (joyY<430){
         direcaoY=1;
         enableY=0;
         if (StepDriverXY==1){
@@ -136,38 +147,54 @@ void jog(){
         }
             
     }
-    if(joyY<550 && joyY>450){
+    if(joyY<550 && joyY>430){
         enableY=1;
     }
+    //PARA Z
+
+    if (BotaoZcima == 0) {
+        // Move Z up
+        direcaoZ = 0;
+        enableZ = 0;
+
+
+    } else if (BotaoZbaixo == 0) {
+        // Move Z down
+        direcaoZ = 1;
+        enableZ = 0;
+        
+
+    } else {
+        // Both buttons not pressed, stop movement
+        enableZ = 1;
+    }
+
+
+
 }
 
 
 
 int main() {
     wait(1);
-    toggle.attach(&flip, 0.001); // intervalo de 1/1000 de segundo
-    
-    
+    toggle.attach(&flip, 0.001); // Interval of 1/1000 of a second
+
     enableX = 1;
     enableY = 1;
     enableZ = 1;
     StepDriverXY = 1;
-    //StepDriverZ = 1;
-    
+
     while (1) {
-        if (i==0){ //referenciamento
-            refEixoX();
+        if (i == 0) { // Reference positioning
+            /*refEixoX();
             refEixoY();
-            refEixoZ();
-            i=1;
+            refEixoZ();*/
+            i = 1;
         }
-        /*if (i==1){ //jog
+
+        if (i == 1) { // Manual control (jog)
             jog();
-            printf("\r X=%4d ",posX);
-        }*/
-
-        
-        
-
+            printf("\r X=%4d ", posX); // Assuming you want to display posX, change if needed
+        }
     }
 }
